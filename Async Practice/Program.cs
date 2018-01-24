bc922fcf4814187ef8192bf041531591873da8f0
Mahesh <mahesh.verma1983@gmail.com>
2018-01-24 22:13:06 +0530

    Initial commit

---
+++ Async Practice/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Async_Practice
+{
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            Myfunc();
+            Console.WriteLine("Main thread over.");
+            Thread.Sleep(15000);
+        }
+        public static void Myfunc()
+        {
+            Console.WriteLine("Calling Function LongWaited()");
+            Task.Run( new Action(LongWait));
+            //LongWait();
+            Console.WriteLine("Function LongWaited() called");
+            int i = 9;
+            i++;
+        }
+        public static async void Myasyncfunc()
+        {
+            Console.WriteLine("Calling Function LongWait()");
+            await Task.Run( new Action(LongWait));
+            //LongWait();
+            Console.WriteLine("Function LongWait () called");
+        }
+        public static void LongWait()
+        {
+            Thread.Sleep(10000);
+            Console.WriteLine("Task Complete");
+        }
+    }
+}
---
+++ Async Practice/TPL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Async_Practice
+{
+    // Task parallel library. Function will optimally use the core of the machines.
+    class TPL
+    {
+        static void Main(string[] args)
+        {
+            Parallel.For(0, 1000000, (x) => RunMillionIterations());
+        }
+        public static void RunMillionIterations()
+        {
+            string x = "";
+            for(int i=0;i<1000000;i++)
+            {
+                x = x + "s";
+            }
+        }
+    }
+}
